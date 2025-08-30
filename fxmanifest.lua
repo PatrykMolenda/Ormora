a7a39daef17a82b0e0cbdec69336a71e936acf88
FrancuzDEV <biznesmonte@gmail.com>
2025-08-30 16:09:07 +0200

    fix

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ fx_version 'cerulean'
 name 'Ormora'
 description 'Lightweight ORM Library for Lua 5.4 in FiveM'
 author 'PatrykMolenda'
-repository 'PatrykMolenda/ormora'
+repository 'PatrykMolenda/Ormora'
 version '0.1.0'
 
 lua54 'yes'
